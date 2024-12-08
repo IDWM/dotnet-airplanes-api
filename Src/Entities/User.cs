d5734d52b5e2bb3c47f00279e2f0993da55f27ac
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-07 23:03:46 -0300

    feat: add user roles navigation property

--- Src/Entities/User.cs
+++ Src/Entities/User.cs
@@ -2,5 +2,8 @@ using Microsoft.AspNetCore.Identity;
 
 namespace dotnet_airplanes_api.Src.Entities
 {
-    public class User : IdentityUser<int> { }
+    public class User : IdentityUser<int>
+    {
+        public ICollection<UserRole> UserRoles { get; set; } = [];
+    }
 }
