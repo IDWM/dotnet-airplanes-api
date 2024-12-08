006d3657cdd8db2de1699eb5b878c7259a879377
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-07 23:04:02 -0300

    feat: setup identity service configuration

--- Program.cs
+++ Program.cs
@@ -1,5 +1,44 @@
+using dotnet_airplanes_api.Src.Data;
+using dotnet_airplanes_api.Src.Entities;
+using dotnet_airplanes_api.Src.Validators;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite("Data Source=app.db");
+});
+builder
+    .Services.AddIdentityCore<User>(opt =>
+    {
+        opt.Password.RequireNonAlphanumeric = false;
+        opt.Password.RequiredLength = 8;
+        opt.Password.RequiredUniqueChars = 0;
+
+        opt.User.RequireUniqueEmail = true;
+        opt.User.AllowedUserNameCharacters =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+ ";
+    })
+    .AddRoles<Role>()
+    .AddRoleManager<RoleManager<Role>>()
+    .AddEntityFrameworkStores<DataContext>()
+    .AddPasswordValidator<CustomPasswordValidator>();
+
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
