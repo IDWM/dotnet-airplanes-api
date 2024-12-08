e3acaec2c25b88222b3607bf499c94ad8836f068
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-07 23:03:10 -0300

    feat: implement database model configuration

--- Src/Data/DataContext.cs
+++ Src/Data/DataContext.cs
@@ -18,5 +18,46 @@ namespace dotnet_airplanes_api.Src.Data
         >(options)
     {
         public required DbSet<Airplane> Airplanes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // User configuration
+            builder.Entity<User>(b =>
+            {
+                // Email y Username configuration
+                b.HasIndex(u => u.NormalizedEmail).HasDatabaseName("EmailIndex").IsUnique();
+
+                b.HasIndex(u => u.NormalizedUserName)
+                    .HasDatabaseName("UserNameIndex")
+                    .IsUnique(false);
+
+                // User roles configuration
+                b.HasMany(ur => ur.UserRoles)
+                    .WithOne(u => u.User)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+
+            // Role configuration
+            builder.Entity<Role>(b =>
+            {
+                b.HasMany(ur => ur.UserRoles)
+                    .WithOne(u => u.Role)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                // Seed roles
+                b.HasData(
+                    new Role
+                    {
+                        Id = 1,
+                        Name = "Admin",
+                        NormalizedName = "ADMIN"
+                    }
+                );
+            });
+        }
     }
 }
